bdd520f5ba8e48bbe7b6b91d8e266f98315b8210
Filip <filip373@gmail.com>
2016-05-25 15:32:12 +0200

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#home'
   namespace :api, defaults: {format: :json} do
     resources :contracts, except: [:edit]
     resources :showrooms, only: [:index, :show]
