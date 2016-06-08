5e912f02aa96171bb73c219df1f5ff9e2a169094
Filip <filip373@gmail.com>
2016-06-08 15:36:55 +0200

    synchronization table keeping only last sync and all non-empty syncs

--- app/models/synchronization.rb
+++ app/models/synchronization.rb
@@ -4,16 +4,19 @@ class Synchronization < ActiveRecord::Base
     central = CentralShowrooms.new(ENV['CENTRAL_SYNC_URL'])
     date = Time.now
     operations = nil
-    if Synchronization.any?
-      operations = Showroom.synchronize(central.updated_after(last_date))
+    last_sync = last_by_date
+    unless last_sync.nil?
+      operations = Showroom.synchronize(central.updated_after(last_sync.date))
     else
       operations = Showroom.synchronize(central.all)
     end
-    Synchronization.create!(date: date, created: operations[:created], updated: operations[:updated])
+    Synchronization.create!(date: date, created: operations[:created],
+      updated: operations[:updated])
+    last_sync.destroy if !last_sync.nil? && last_sync[:created] == 0 && last_sync[:updated] == 0
   end
 
-  def Synchronization.last_date
-    Synchronization.select(:date).order(date: :desc).first.date
+  def Synchronization.last_by_date
+    Synchronization.order(date: :desc).first
   end
 
 end
